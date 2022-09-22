8ebfdbb592424c3a988d9a7cf3a33c267c209208
Christian Téllez <ch.tellez@hotmail.com>
2022-09-22 00:39:47 -0500

    Finalizando ajustes en configuración goland

--- restWebSockets/server/server.go
+++ restWebSockets/server/server.go
@@ -9,30 +9,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//Struct with parameters needed to start server
+// Config Struct with parameters needed to start server
 type Config struct {
-	Port 		string
-	JWTSecret 	string
+	Port        string
+	JWTSecret   string
 	DatabaseUrl string
 }
 
-//Interface needed to start a new server
+// Server Interface needed to start a new server
 type Server interface {
 	Config() *Config
 }
 
-//Broker that handle the conexion to the server
+// Broker that handle the connexion to the server
 type Broker struct {
 	config *Config
 	router *mux.Router
 }
 
-//Broker method for setting up a new Broker
+// Config Broker method for setting up a new Broker
 func (b *Broker) Config() *Config {
 	return b.config
 }
 
-//func that validates the config needed to create a new Broker object
+// NewServer func that validates the config needed to create a new Broker object
 func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 	//Port, JWTSecret and DatabaseUrl must be specified
 	if config.Port == "" {
@@ -53,11 +53,11 @@ func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 	return broker, nil
 }
 
-//broker method that starts a new server
-func (b *Broker) Start(binder func (s Server, r *mux.Router)){
+// Start broker method that starts a new server
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
 	b.router = mux.NewRouter()
 	binder(b, b.router)
-	log.Println("Startting server on port", b.Config().Port)
+	log.Println("Starting server on port", b.Config().Port)
 	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
 		log.Fatal("ListenAndServe error: ", err)
 	}
