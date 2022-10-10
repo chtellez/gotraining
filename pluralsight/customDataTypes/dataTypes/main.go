1f785c989ed1241ae6c1f180d8fb2a2c26c7f48a
Christian Tellez <ch.tellez@hotmail.com>
2022-10-10 18:43:32 -0500

    Finalizando Types Aliases y Types Definitions

--- pluralsight/customDataTypes/dataTypes/main.go
+++ pluralsight/customDataTypes/dataTypes/main.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	p := organization.NewPerson("Christian", "Téllez")
-	err := p.SetTwitterHandler("@ctellez")
+	err := p.SetTwitterHandler("@tellez")
+	fmt.Printf("%T\n", organization.TwitterHandler("test"))
 	if err != nil {
 		fmt.Printf("An error ocurred setting twitter handler: %s\n", err.Error())
 	}
 	println(p.ID())
 	println(p.FullName())
 	println(p.TwitterHandler())
+	println(p.TwitterHandler().RedirectUrl())
 }
