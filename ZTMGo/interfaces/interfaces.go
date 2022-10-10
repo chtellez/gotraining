05f5697e962498b6bc1a65c9a50f48ef927c1568
Christian Tellez <ch.tellez@hotmail.com>
2022-10-10 17:34:44 -0500

    interfaces first part

---
+++ ZTMGo/interfaces/interfaces.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Preparer interface {
+	PrepareDish()
+}
+
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("Cook Chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("Chop Salad")
+	fmt.Println("add dressing")
+}
+
+func PrepareDishes(dishes []Preparer){
+	fmt.Println("Prepare dishes")
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish: %v--\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
+
+func main() {
+    dishes := []Preparer{Chicken("Chicken Wings"), Salad("Iceberg Salad")}
+	PrepareDishes(dishes)
+}
