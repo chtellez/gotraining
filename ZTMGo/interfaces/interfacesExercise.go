e8351f964c7a5f969408ca727b5f6881fa00f79a
Christian Tellez <ch.tellez@hotmail.com>
2022-11-06 10:18:48 -0500

    Finalizando ejercicio interfases

--- ZTMGo/interfaces/interfacesExercise.go
+++ ZTMGo/interfaces/interfacesExercise.go
@@ -9,25 +9,88 @@ import "fmt"
 
 //--Requirements:
 
-type LiftAsigner interface {
-	AsignLift
+const (
+	SmallLift = iota
+	StandarLift
+	LargeLift
+)
+
+
+const (
+	Motorcycles = iota
+	Cars
+	Trucks
+)
+type Lift struct {
+	name string
+	liftType int
 }
 
+var lifts = []Lift{
+	{name: "SmallLift", liftType: SmallLift},
+	{name: "StandarLift", liftType: StandarLift},
+	{name: "LargeLift", liftType: LargeLift},
+}
+
+type LiftAsigner interface {
+	LiftPicker()
+	PrintLift()
+}
 //* Vehicles have a model name in addition to the vehicle type:
 //  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
 type Vehicle struct {
-
+	vType int
+	model string
+	lift Lift
+	LiftAsigner
 }
 
 //* The shop has lifts for multiple vehicle sizes/types:
 //  - Motorcycles: small lifts
 //  - Cars: standard lifts
 //  - Trucks: large lifts
+
+func (v *Vehicle) LiftPicker(){
+	switch v.vType {
+	case Motorcycles:
+		v.lift = lifts[SmallLift]
+	case Cars:
+		v.lift = lifts[StandarLift]
+	case Trucks:
+		v.lift = lifts[LargeLift]
+	default:
+		fmt.Println("Vehicle type not handle")
+	}
+
+}
+
+func (v *Vehicle) PrintLift() {
+	fmt.Println(v.model, "assigned to", v.lift.name )
+}
+
 //* Write a single function to handle all of the vehicles
 //  that the shop works on.
+
+func assignLift( vehicles []LiftAsigner) {
+	for i := 0; i < len(vehicles); i++ {
+		vehicles[i].LiftPicker()
+		vehicles[i].PrintLift()
+	}
+}
 
 //* Direct at least 1 of each vehicle type to the correct
 //  lift, and print out the vehicle information.
 //
+func main (){
+	
+	mazda := Vehicle{model:"Mazda", vType:Cars}
+	Kawa := Vehicle{model:"Kawasaky", vType:Motorcycles}
+	Kentworth := Vehicle{model:"Kentworth", vType:Trucks}
+
+	vehicles := []LiftAsigner{&mazda, &Kawa, &Kentworth}
+
+	assignLift(vehicles)
+}
+
 //--Notes:
 //* Use any names for vehicle models
